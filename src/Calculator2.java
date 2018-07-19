acb6588e945afbb15b4cb2ea35b79bb551755c7d
이창훈 <@DESKTOP-AVG9VCQ.local>
2018-07-19 17:02:16 +0900

    Calculator 2

---
+++ src/Calculator2.java
@@ -0,0 +1,11 @@
+
+public class Calculator2 {
+	public static void main(String[] args)
+	{
+		System.out.println("µ¡¼À : " + (3 + 4));
+		System.out.println("»¬¼À : " + (4 - 3));
+		System.out.println("°÷¼À : " + (4 * 3));
+		System.out.println("³ª´°¼À : " + (3 / 4));
+	}
+
+}
