81243ca38c2bca3bf2e9270af24c0569526a80b1
lch2623100 <41418878+lch2623100@users.noreply.github.com>
2018-07-19 17:30:40 +0900

    Add files via upload

---
+++ Calculator2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Calculator2 {
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ã¹ ¹øÂ° ÀÔ·Â °ª : ");
+		int first = scanner.nextInt();
+		System.out.println(first);
+		
+		System.out.println("µÎ ¹øÂ° ÀÔ·Â °ª : ");
+		int second = scanner.nextInt();
+		System.out.println(second);
+		
+		System.out.println("µ¡¼À : " + (first + second));
+		System.out.println("»¬¼À : " + (first - second));
+		System.out.println("°÷¼À : " + (first * second));
+		System.out.println("³ª´°¼À : " + (first / second));
+	}
+
+}
